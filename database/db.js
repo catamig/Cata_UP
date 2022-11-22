a8feeaab5bbe687149acb1922844a73f3604961b
catamig <96384844+catamig@users.noreply.github.com>
2022-11-22 18:02:19 -0300

    Update db.js

--- database/db.js
+++ database/db.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '1234',
-    database: 'crud_nodejs_mysql'
+    database: 'crud_nodejs-master'
 });
 
 connection.connect((error)=>{
